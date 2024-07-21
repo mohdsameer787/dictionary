9b9d9c3ad12e1c6168075f14e51815b5f1491245
mohdsameer787 <161015980+mohdsameer787@users.noreply.github.com>
2024-07-21 00:52:14 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -8,9 +8,11 @@ let data = await fetch(`https://api.dictionaryapi.dev/api/v1/entries/en/${word}`
 let info =  await data.json();
 console.log(info);
 const meanings =info[0].meaning;
+let dis=document.querySelector('h1');
+
 for (let index = 0; index < meanings.noun.length; index++) {
    console.log(`defination:${meanings.noun[index].definition}`);
-   
+   dis.innerHTML=`defination:${meanings.noun[index].definition}`
 }
 
 
